6fb77934a77c04e7a1f9f1bfbb58df2a967d4ea9
anfedu <firdausnurilahmad@gmail.com>
2022-05-31 21:26:06 +0800

    better coding academy 14

--- chat-app/src/components/Root/initialised/Login/Login.tsx
+++ chat-app/src/components/Root/initialised/Login/Login.tsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+`;
+
+const Form = styled.form`
+  margin: auto;
+`;
+
 function Login({}) {
-  return <h1>Login</h1>;
+  return (
+    <Wrapper>
+      <Form>Login</Form>
+    </Wrapper>
+  );
 }
 
 export default Login;
